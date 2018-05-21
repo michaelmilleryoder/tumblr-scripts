cc8b4b0927381e7519c2ab2fc58d4232f636cccb
Michael Miller Yoder <michaelmilleryoder@gmail.com>
2018-05-21 18:11:52 -0400

    Added bootstrapped terms to identity category annotation

--- annotate_identity_category_mentions.py
+++ annotate_identity_category_mentions.py
@@ -15,6 +15,7 @@ class IdentityAnnotator():
         self.states_path = os.path.join(data_dirpath, 'states.csv')
         self.nationalities_path = os.path.join(data_dirpath, 'nationalities.txt')
         self.ethnicities_path = os.path.join(data_dirpath, 'ethnicities.txt')
+        self.countries_path = os.path.join(data_dirpath, 'countries.txt')
         self.terms_path = os.path.join(data_dirpath, 'search_terms.pkl')
         self.excl_terms_path = os.path.join(data_dirpath, 'excl_terms.pkl')
         self.terms, self.excl_terms, self.terms_re = self._pattern()
@@ -33,9 +34,13 @@ class IdentityAnnotator():
         with open(self.ethnicities_path) as f:
             eths = [e.split()[0].lower() for e in f.read().splitlines() if (len(e.split()[0]) > 4 and not e.split()[0].lower() in outlist)]
 
+        # Load countries
+        with open(self.countries_path) as f:
+            countries = [c.lower() for c in f.read().splitlines()]
+
         # Regex patterns
         terms = {
-                'age': [r'(?:[^-+\w]|^)([1-6]{1}[0-9]{1})[^-+0-9]|^([1-6]{1}[0-9]{1})$',
+                'age': [r'(?:[^-+\w]|^)([1-5]{1}[0-9]{1})[^-+0-9]|^([1-5]{1}[0-9]{1})$',
                        r'twelve',
                        r'thirteen',
                        r'fourteen',
@@ -45,19 +50,32 @@ class IdentityAnnotator():
                        r'eighteen',
                        r'nineteen',
                         r'(twenty|thirty|forty|fifty)([ -](one|two|three|four|five|six|seven|eight|nine))?',
+                        r'\bold\b',
+                        r'\byears\b',
+                        r'\bage\b',
+                        r'\by\/o\b', r'\by\.o\b'
+                        r'\bkid\b',
                        ],
-        #         'location': [],
+                'location': [r'\b{}\b'.format(el) for el in countries + states] + [
+                        r'\buk\b', r'\busa\b', r'\bus\b', r'méxico', r'\baus\b', r'\bmx\b', r'\bnz\b',
+                        r'\bca\b', r'\btx\b', r'\bfl\b', r'\bnc\b', r'\bpa\b', r'\bnj\b', r'\bnc\b', r'\bcali\b', r'\baz\b', r'\bal\b', r'\bsocal\b', r'\bwa\b', r'\bva\b', r'\bga\b',
+                        r'london', r'chicago', r'\bdc\b', r'\bny\b', r'\bnyc\b', r'new york', r'\bsan\b', r'istanbul', r'toronto', r'\batl\b', r'\bpnw\b',
+                        r'north', r'south', r'east', r'west',
+                        ],
                 'gender': [r'male\b', r'female', 
                             r'trans', r'ftm', r'mtf', r'\bcis',
                             r'girl\b', r'boy\b', r'\bman\b', r'guy\b', r'woman', r'gu+rl', r'gii+rl',
                             r'non-binary', r'nonbinary', r'\bnb\b', r'agender', r'neutrois',
-                            r'\bmom\b', r'\bdad\b', r'wife', r'husband', r'\bbrother\b', r'\bson\b', r'\bsister\b',
-                            r'bigender', r'lgbt', r'genderfluid', r'gender-fluid'],
+                            r'\bmom\b', r'mum', r'\bdad\b', r'wife', r'husband', r'\bbrother\b', r'\bson\b', r'\bsister\b',
+                            r'bigender', r'lgbt', r'genderfluid', r'gender-fluid',
+                            r'princess', r'queen', r'lady', r'daughter', r'mommy', # added by bootstrapping
+                            ],
                 'sexual orientation': 
                              [r'gay', r'straight', r'lesbian', r'\bhomo',
                                r'bisexual', r'\bbi\b', r'pansexual', r'\bpan\b',
                                r'lgbt', r'queer',
                                r'\bace\b', r'\basexual', r'aro-ace', r'aro/ace',
+                                r'demisexual', # added by bootstrapping
                              ],
                  'pronouns': [
                      r'(?:\W|\b)she(?:\W|\b)', r'(?:\W|\b)her(?:\W|\b)',
@@ -71,6 +89,9 @@ class IdentityAnnotator():
                     r'extrovert', 
                     r'ambivert',
                     r'\b[0-9]w[0-9]\b',
+                    r'\baries\b', r'\btaurus\b', r'\bgemini\b', r'\bcancer\b', r'\bleo\b', r'\bvirgo\b', r'\blibra\b', r'\bscorpius\b',
+                    r'slytherin', r'ravenclaw', r'hufflepuff', r'gryffindor', # added by bootstrapping
+                    r'neutral', r'chaotic', r'lawful', # added by bootstrapping
                     ],
                 'ethnicity/nationality': [r'\b{}\b'.format(el) for el in eths + nats] + 
                         [r'latino', r'latina', r'cubana', r'cubano', r'chilena', r'chileno', r'mexicano', r'mexicana',
@@ -78,7 +99,38 @@ class IdentityAnnotator():
                 'relationship status': [
                     r'taken', r'married', r'single', r'engaged', r'husband', r'spouse', r'wife', r'newlywed',
                     r'in a rl', r'in rl', r'in a relationship',
-                ]
+                ],
+                'roleplay': [
+                    r'roleplay', r'\brp\b', r'selective', r'semi-selective', r'm!a',
+                ],
+                'fandoms': [
+                    r'multi', r'fan', r'kpop', r'fandom', r'marvel', r'fandoms', r'stan', r'obsessed', r'multi-fandom',
+                    r'fanatic',
+                    r'k-pop', r'bts', r'exo', r'army', r'got7', r'yuri', r'undertale', r'yaoi', r'riverdale', r'5sos', r'bnha',
+                    r'star wars', r'rey', r'universe', r'multiverse', r'canon', r'verse',
+                    r'voltron', r'otaku', r'shipper', r'ships', r'sims', r'multiship',
+                    r'swiftie',
+                    r'phan',
+                    r'hamilton',
+                    r'series',
+                    r'potterhead', r'hp', r'harry potter',
+                    r'pokemon',
+                    r'sherlock',
+                    r'disney',
+                ],
+                'interests': [
+                    r'arts?', r'music', r'anime', r'books', r'photography', r'fashion', r'memes', r'nature', r'food',
+                    r'coffee', r'\bcats?\b', r'aesthetics?', r'games', r'draw', r'writing', r'space', r'book', r'travel',
+                    r'tv', r'poetry', r'friends', r'write', r'manga', r'pics', r'penned', r'horror', r'kitten', r'design',
+                    r'tattoos', r'movies', r'dogs?', r'tea', r'bands', r'makeup', r'animals?', r'beauty', r'memes?', 
+                    r'drawing', r'pop', r'metal', r'fitness', r'history', r'science', r'film', r'gifs', r'photos', r'reading',
+                    r'comics', r'lifestyle', r'weed', r'water', r'soccer', r'gaming', r'hair', r'family', r'pictures',
+                    r'psychology', r'cosplay', r'theatre', r'law', r'pizza',
+                ],
+                'weight': [
+                    r'\b(?:c|g|s|h|l)w[1-3]?\b', r'\blbs?\b', r'\bkgs?\b', r'pounds', r'kilograms',
+                    r'weight', r'fat', r'thin', r'ana',
+                ],
         }
         terms['sexuality/gender'] = terms['gender'] + terms['sexual orientation'] + terms['pronouns']
 
@@ -107,7 +159,7 @@ class IdentityAnnotator():
             'sexual orientation': [
                 'ace maverick',
                 ],
-            'ethnicity/nationlity': [
+            'ethnicity/nationality': [
                 'sioux falls',
                 'black lives matter',
                 'blacklivesmatter',
